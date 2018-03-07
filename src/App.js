e1483757ac53cb08a634afd1ab4b53b8ec13eafa
MaxDrljic <max_spree@hotmail.com>
2018-03-07 13:07:57 +0100

    Added option to change between campers in 30 days and All Time

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Table from 'react-bootstrap/lib/Table';
 import Image from 'react-bootstrap/lib/Image';
+import 'font-awesome/css/font-awesome.css';
 
 import './App.css';
 
@@ -9,7 +10,8 @@ class App extends Component {
 
   state = {
     top100Days: [],
-    top100AllTime: []
+    top100AllTime: [],
+    current: true
   }
 
   getFCCData(url, stateName) {
@@ -20,13 +22,19 @@ class App extends Component {
       });
   }
 
+  pointChange(value) {
+    if (this.state.current !== value) {
+      this.setState({ current: value });
+    }
+  }
+
   componentDidMount() {
     this.getFCCData('https://fcctop100.herokuapp.com/api/fccusers/top/recent', "top100Days");
     this.getFCCData('https://fcctop100.herokuapp.com/api/fccusers/top/alltime', "top100AllTime");
   }
 
   render() {
-    const { top100Days, top100AllTime } = this.state;
+    const { top100Days, top100AllTime, current } = this.state;
     return (
       <div className="App container">
         <Table striped bordered condensed hover className="colorBlack">
@@ -34,12 +42,12 @@ class App extends Component {
             <tr>
               <th>#</th>
               <th>Camper Name</th>
-              <th>Points in 30 Days</th>
-              <th>All Time Points</th>
+              <th className="points30Days" onClick={(event)=> this.pointChange(true)}>Points in 30 Days {current && (<i className="fa fa-caret-down"></i>)}</th>
+              <th className="allTimePoints" onClick={(event)=> this.pointChange(false)}>All Time Points {current === false && (<i className="fa fa-caret-down"></i>)}</th>
             </tr>
           </thead>
           <tbody>
-            {top100Days.map((row, index) => (
+            {current && (top100Days.map((row, index) => (
               <tr key={row.username}>
                 <td>{index + 1}</td>
                 <td><a href={`https://www.freecodecamp.org/${row.username}`}>
@@ -49,7 +57,19 @@ class App extends Component {
                 <td>{row.alltime}</td>
               </tr>
             )
-          )}
+          ))}
+
+          {current === false && (top100AllTime.map((row, index) => (
+            <tr key={row.username}>
+              <td>{index + 1}</td>
+              <td><a href={`https://www.freecodecamp.org/${row.username}`}>
+                <Image src={row.img} className="imgHeight" circle /> {row.username}
+              </a></td>
+              <td>{row.recent}</td>
+              <td>{row.alltime}</td>
+            </tr>
+          )
+        ))}
           </tbody>
         </Table>
       </div>
